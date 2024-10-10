eb7d85155b4224f6dfee99e0283269694fb5594b
qdchuran <qdit@vip.qq.com>
2024-10-10 13:59:13 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.70h.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "CznkJ042Hc89iHvqGXERWCR1x68HMJxGsyXjK8VuHdM=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.imking.net"];
+pub const PUBLIC_RS_PUB_KEY: &str = "eVFShZlTstB0vz4f6ec30BqYyTZzWUbOBapo9BXgtco=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
